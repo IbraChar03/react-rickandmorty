2f03a9c72aac7bbf315d42a1f9bf8e615e107f13
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-03-04 15:43:08 +0100

    cambiamento da component class a function class

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react'
-class Card extends Component {
-    render() {
-        return (
-            <>
-                <div className="card" >
-                    <img src={this.props.card.image} alt="" />
-                    <div className="text">
-                        <div className="name">{this.props.card.name}</div>
-                        <div className="status">{this.props.card.status}</div>
-                        <div className="species">{this.props.card.species}</div>
-                    </div>
+// import React, { Component } from 'react'
+// class Card extends Component
+function Card(props) {
+    // render() {
+    return (
+        <>
+            <div className="card" >
+                <img src={props.card.image} alt="" />
+                <div className="text">
+                    <div className="name">{props.card.name}</div>
+                    <div className="status">{props.card.status}</div>
+                    <div className="species">{props.card.species}</div>
                 </div>
+            </div>
 
-            </>
-        );
-    }
+        </>
+    );
+    // }
 
 }
 export default Card;
